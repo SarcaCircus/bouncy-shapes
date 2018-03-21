32ca4342ed768f337a46e05cda8bc6808117396e
Jackwelyne <36117873+SarcaCircus@users.noreply.github.com>
2018-03-21 12:33:42 -0700

    Update Scene.cpp

--- Scene.cpp
+++ Scene.cpp
@@ -1,5 +1,28 @@
-#include "Movement.h"
+
 #include "Scene.h"
+#pragma once
+#include <vector>
+#include <memory>
+#include "GameObject.h"
+#include <d2d1.h>
+
+
+class SquareMoves {
+
+public:
+	void moveGreenSquare( double deltaTime) {
+	
+			auto thun3 = _gameObjects;
+			MPoint2F start = thun3.getLocation();
+			thun3._location = { start.x / (float)deltaTime * 17,start.y };
+			_gameObjects=thun3;
+		
+	}
+private:
+	GameObject _gameObjects;
+	double deltaTime;
+
+};
 
 
 void Scene::insertGameObject(std::unique_ptr<GameObject>& object)
@@ -11,12 +34,31 @@ void Scene::insertGameObject(std::unique_ptr<GameObject>& object)
 void Scene::Update(double deltaTime)
 {
 	
-	//auto thun3 = *_gameObjects.at(5);
-	//MPoint2F start = thun3.getLocation();
-	//thun3._location = { start.x /(float)deltaTime*17,start.y };
-	//*_gameObjects.at(5) = thun3;
+	auto thon3 = *_gameObjects.at(7);
+	MPoint2F art = thon3.getLocation();
+	thon3._location = { art.x /(float)deltaTime*15,art.y / (float)deltaTime * 15 };
+	*_gameObjects.at(7) = thon3;
 	
-	//auto one = moveGreenSquare(*_gameObjects.at(5),deltaTime);
+	auto redun = *_gameObjects.at(4);
+	MPoint2F at = redun.getLocation();
+	redun._location = { at.x / (float)deltaTime * 20,at.y / (float)deltaTime *15};
+	*_gameObjects.at(4) = redun;
+
+	auto reduin = *_gameObjects.at(9);
+	MPoint2F mat = reduin.getLocation();
+	reduin._location = { mat.x / (float)deltaTime * 20,mat.y / (float)deltaTime * 15 };
+	*_gameObjects.at(9) = reduin;
+	
+	auto thun1 = *_gameObjects.at(2);
+	MPoint2F stt = thun1.getLocation();
+	thun1._location = { stt.x ,stt.y / (float)deltaTime * 15 };
+	*_gameObjects.at(1) = thun1;
+	
+	auto thun3 = *_gameObjects.at(5);
+	MPoint2F sart = thun3.getLocation();
+	thun3._location = { sart.x / (float)deltaTime * 17,sart.y };
+	*_gameObjects.at(5) = thun3;
+    //moveGreenSquare(deltaTime);
 	auto thun4 = *_gameObjects.at(6);
 	MPoint2F star = thun4.getLocation();
 	if (star.y == 486 || star.y==0) {
@@ -64,3 +106,4 @@ void Scene::Draw(ID2D1HwndRenderTarget & target)
 		renderer.Render(target, object->getLocation());
 	}
 }
+
